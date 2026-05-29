381da8aec0c9bda538c0db0a61c44202d13527ca
gauravbshet <157151025+gauravbshet@users.noreply.github.com>
2026-05-29 14:50:47 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        s.erase(remove_if(s.begin(), s.end(), [](char c){ return !isalnum(c); }), s.end());
+        int n = s.length();
+        for(int i=0;i<n/2;i++){
+            if(s[i]!=s[n-1-i]){
+            return false;
+            }
+        }
+        return true;
+    }
+};
