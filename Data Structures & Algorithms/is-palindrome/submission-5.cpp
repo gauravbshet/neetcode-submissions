edd465adab3c54765a424804da6a504f2bb8d1c5
Gaurav B Shet <157151025+gauravbshet@users.noreply.github.com>
2026-06-29 12:13:58 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       transform(s.begin(),s.end(),s.begin(),::tolower);
+       s.erase(remove_if(s.begin(),s.end(),[](char c){return !isalnum(c);}),s.end());
+        int n=s.size();       
+        for(int i=0;i<n;i++){
+            if(s[i]!=s[n-i-1]){
+                return false;
+            }
+        }
+        return true;
+       
+    }
+};
